303c1a3a99a62cab86ebe80bfd29ad4ad5dd3862
dNaszta <david.nasztanovics@stylersonline.com>
2023-11-30 17:48:30 +0100

    add dsl solution too

--- integr/src/main/java/com/bogisolutions/integr/IntegrApplicationConfig.java
+++ integr/src/main/java/com/bogisolutions/integr/IntegrApplicationConfig.java
@@ -1,15 +1,29 @@
 package com.bogisolutions.integr;
 
+// import com.bogisolutions.integr.service.RegistrationService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.channel.DirectChannel;
+// import org.springframework.integration.dsl.IntegrationFlow;
+// import org.springframework.integration.dsl.MessageChannels;
 import org.springframework.messaging.MessageChannel;
 
 @Configuration
 public class IntegrApplicationConfig {
-    @Bean
+    @Bean("registrationRequest")
     public MessageChannel registrationRequest() {
         return new DirectChannel();
     }
 
+    /*
+     * if we use DSL instead of Annotation we can remove the @ServiceActivator annotation from the RegistrationService class
+     */
+    /*
+    @Bean
+    public IntegrationFlow integrationFlow(RegistrationService registrationService) {
+        return flow -> flow
+                .channel(registrationRequest())
+                .handle(registrationService, "register");
+    }
+     */
 }
